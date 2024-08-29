975c17226ea89a61e661d023810a2a73a64ffcb9
Sergey Lapshin <sp.lapshin@gmail.com>
2024-08-29 16:53:21 +0200

    added reading parameters for storage

---
+++ src/Microservice/StorageSettings.cs
@@ -0,0 +1,9 @@
+namespace Microservice;
+
+public class StorageSettings
+{
+    public string? Endpoint { get; set; }
+    public string? BucketName { get; set; }
+    public string? AccessKey { get; set; }
+    public string? SecretKey { get; set; }
+}
