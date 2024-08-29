e80f68825e616a60a72ce9a1ce40af550e44ea90
Sergey Lapshin <sp.lapshin@gmail.com>
2024-08-29 16:53:21 +0200

    added reading parameters for storage

--- src/Microservice/EndpointRouteBuilderExtensions.cs
+++ src/Microservice/EndpointRouteBuilderExtensions.cs
@@ -1,25 +1,34 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Options;
 
 namespace Microservice;
 
 public static class EndpointRouteBuilderExtensions
 {
     private static readonly string[] HttpHeadMethods = ["HEAD"];
+
+    public static void RegisterConfigEndpoints(this IEndpointRouteBuilder app)
+    {
+        var configEndpoints = app.MapGroup("/config");
+
+        configEndpoints.MapGet("", (IOptions<StorageSettings> settings) => Results.Ok(settings.Value)).WithOpenApi();
+    }
+
     public static void RegisterFileEndpoints(this IEndpointRouteBuilder app)
     {
         var fileEndpoints = app.MapGroup("/file");
 
         // GET file/1
         fileEndpoints.MapGet("{objectId}", (string objectId) => Results.Ok()).WithOpenApi();
-        
+
         // HEAD file/1
         fileEndpoints.MapMethods("{objectId}", HttpHeadMethods, (string objectId) => Results.Ok()).WithOpenApi();
-        
+
         // PUT file/1
         fileEndpoints.MapPut("{objectId}", (string objectId) => Results.Ok()).WithOpenApi();
-        
+
         // DELETE file/1
         fileEndpoints.MapDelete("{objectId}", (string objectId) => Results.Ok()).WithOpenApi();
     }
--- src/Microservice/Program.cs
+++ src/Microservice/Program.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microservice;
@@ -10,29 +11,33 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        
+
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddProblemDetails();
+
+        builder.Configuration.AddEnvironmentVariables();
+        builder.Services.Configure<StorageSettings>(builder.Configuration.GetSection("STORAGE"));
 
         var app = builder.Build();
 
         app.UseSwagger();
         app.UseSwaggerUI();
-        
+
         app.UseExceptionHandler();
         app.UseStatusCodePages();
 
         app.UseHttpsRedirection();
-        
+
+        app.RegisterConfigEndpoints();
         app.RegisterFileEndpoints();
-        
+
         app.MapFallback(async Task (context) =>
         {
             context.Response.StatusCode = StatusCodes.Status404NotFound;
-            await context.Response.WriteAsync($"The endpoint you are looking for does not exist!");
+            await context.Response.WriteAsync("The endpoint you are looking for does not exist!");
         });
-        
+
         app.Run();
     }
 }
